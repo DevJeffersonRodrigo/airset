f0df60f19e163ea7d37546db0218f743a42bcbcb
oitavohacker <41451403+oitavohacker@users.noreply.github.com>
2018-11-27 01:56:08 -0200

    Add files via upload

---
+++ install_airset.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+# install_airset
+Instalação completa do Airset
+echo "Instalação completa do Airset"
+sudo git clone https://github.com/alefcarvalho/airset.git
+echo ""
+echo "" cd airset
+sudo unzip airset.zip
+echo "" cd airset
+sudo chmod 777 airmon && sudo chmod 777 airset
+sudo ./airset
+echo ""
+sudo apt-get install aircrack-ng -y
+sudo apt-get install aireplay-ng -y
+sudo apt-get install airmon-ng -y
+sudo apt-get install airodump-ng -y
+sudo apt-get install awk -y
+sudo apt-get install bully -y
+sudo apt-get install curl -y
+sudo apt-get install isc-dhcp-server -y
+sudo apt-get install hostapd -y
+sudo apt-get install iwconfig -y
+sudo apt-get install lighttpd -y
+sudo apt-get install macchanger -y
+sudo apt-get install mdk3 -y
+sudo apt-get install nmap -y
+sudo apt-get install php-cgi -y
+sudo apt-get install pyrit -y
+sudo apt-get install python -y
+sudo apt-get install reaver -y
+sudo apt-get install unzip -y
+sudo apt-get install xterm -y
+sudo apt-get install zenity -y
+sudo apt-get install openssl -y
+sudo apt-get install rfkill -y
+sudo apt-get install strings -y
+sudo apt-get install fuser -y
+sudo apt-get install binutils -y
+echo ""
+sudo ./airset
+echo ""
